e70dfb0a63dd4d893ac654d54125cbc02752d22e
shreyas.sn <shreyas.sn@zohocorp.com>
2024-01-06 12:09:02 +0530

    Handle convertor build variant activity not found issue

--- app/src/convertor/kotlin/com/configheroid/android/ConvertorActivity.kt
+++ app/src/convertor/kotlin/com/configheroid/android/ConvertorActivity.kt
@@ -1,17 +1,11 @@
 package com.configheroid.android
 
-import android.graphics.Color
 import android.os.Bundle
-import android.view.View
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.core.view.WindowCompat
-import androidx.lifecycle.ViewModelProvider
-import com.configheroid.android.ad.ui.theme.AdGalaxyTheme
-import com.configheroid.framework.feature.convertor.ui.ConvertorScreen
+import com.cogniheroid.android.BaseActivity
+import com.cogniheroid.framework.feature.convertor.ui.ConvertorScreen
 
-class ConvertorActivity : BaseActitivity() {
+class ConvertorActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
