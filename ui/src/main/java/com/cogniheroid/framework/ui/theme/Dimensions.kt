5fa1ae33931f87f3dafeea33207eb206ba134c8a
shreyas.sn <shreyas.sn@zohocorp.com>
2024-01-28 12:55:27 +0530

    Add generalize dimensions for all UI

---
+++ ui/src/main/java/com/cogniheroid/framework/ui/theme/Dimensions.kt
@@ -0,0 +1,44 @@
+package com.cogniheroid.framework.ui.theme
+
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+object Dimensions {
+
+    val defaultPadding = 16.dp
+
+    val defaultPaddingHalf = 8.dp
+
+    val padding4 = 4.dp
+
+    val padding24 = 24.dp
+
+    val size4 = 4.dp
+
+    val size6 = 6.dp
+
+    val size20 = 20.dp
+
+    val tertiaryFontSize = 12.sp
+
+    val fontSize8 = 8.sp
+
+    val fontSize10 = 10.sp
+
+    val lineHeight = 20.sp
+
+    val letterSpacing = 0.sp
+
+    val size8 = 8.dp
+
+    val size12 = 12.dp
+    val size16 = 16.dp
+
+    val fontSize12 = 12.sp
+
+    val secondaryFontSize = 14.sp
+
+    val primaryFontSize = 16.sp
+
+    val avatarImageSize = 40.dp
+}
